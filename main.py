9ad6e1be8498ee58e113e5d5ac8f6dc57d321780
Harriis10 <66226505+Harriis10@users.noreply.github.com>
2023-08-22 23:55:51 -0400

    main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "harrisnaseeb10@gmail.com")
+email_entry.insert(0, "test@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
